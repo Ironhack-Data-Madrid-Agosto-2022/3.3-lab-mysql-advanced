1b2d0316080d3b1fe0a0d89f5166428fa2a2fc02
Davidmcii <davidmartincasero@hotmail.com>
2022-08-24 20:24:53 +0200

    ejercicio corregido

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,52 +1,57 @@
-
 -- ejercicio 1
-select escritor, sum(suma_sq2)  from
 
-(select titulo, escritor, sum(sales_royalty) as suma_sq2 from 
+	select escritor, sum(suma_sq2+ adv) as profits  from
 
-(select s.ord_num, t.title as titulo, t.royalty, s.qty, t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100 as sales_royalty, a.au_id as escritor
-from sales as s 
+	(select  adv, titulo, escritor, sum(sales_royalty) as suma_sq2 from 
 
-left join titles as t
-on t.title_id=s.title_id
+	(select  t.advance as adv, s.ord_num, t.title as titulo, t.royalty, s.qty, t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100 as sales_royalty, a.au_id as escritor
+	from sales as s 
 
-left join titleauthor as ta
-on t.title_id=ta.title_id
+	left join titles as t
+	on t.title_id=s.title_id
 
-left join authors as a
-on ta.au_id=a.au_id) as ry
+	left join titleauthor as ta
+	on t.title_id=ta.title_id
 
-group by titulo, escritor) as sq2
+	left join authors as a
+	on ta.au_id=a.au_id) as ry
 
-group by escritor;
+	group by titulo, escritor)  as sq2
+
+	group by escritor
+
+	order by profits desc;
+
 
 -- ejercicio 2
+	-- instancia 1
+	create temporary table sales_royalty
 
-create temporary table rollalties
-select s.ord_num, t.title as titulo, t.royalty, s.qty, t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100 as sales_royalty, a.au_id as escritor
-from sales as s 
+	select  t.advance as adv, s.ord_num, t.title as titulo, t.royalty, s.qty, t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100 as sales_royalty, a.au_id as escritor
+	from sales as s 
 
-left join titles as t
-on t.title_id=s.title_id
+	left join titles as t
+	on t.title_id=s.title_id
 
-left join titleauthor as ta
-on t.title_id=ta.title_id
+	left join titleauthor as ta
+	on t.title_id=ta.title_id
 
-left join authors as a
-on ta.au_id=a.au_id;
+	left join authors as a
+	on ta.au_id=a.au_id;
+	-- instancia 2
+	create temporary table royalties
 
-select roll.escritor, sum(sales_royalty) as agg_royalties
+	select sum(sales_royalty) as royalties, escritor, titulo, adv
 
- from rollalties as roll 
- 
- group by roll.escritor;
- 
- -- ejercicio 3
- 
- create table tabla_resumen
-select roll.escritor, sum(sales_royalty) as agg_royalties
+	from sales_royalty
 
- from rollalties as roll 
- 
- group by roll.escritor;
-
+	group by  escritor, titulo;
+	-- instancia 3
+	select escritor, sum(royalties+adv) as profit from royalties group by escritor order by profit asc
+    
+-- ejercicio 3
+	-- instancia 3 anterior modificada- crear la tabla permanente
+    create table most_profiting_authors
+	select escritor, sum(royalties+adv) as profit from royalties group by escritor order by profit asc
+    
+    
